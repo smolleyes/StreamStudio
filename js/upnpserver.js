f2bde9265a076cec8ab7a1cba2cf5f9e2370f4aa
yasmin <s.lagui@gmail.com>
2015-05-28 21:27:02 +0200

    fix

--- js/upnpserver.js
+++ js/upnpserver.js
@@ -575,21 +575,22 @@ function playOnChromecast(currentMedia) {
     ChromecastInterval = setInterval(getChromeCastPos,1000);
     mediaRenderer.on('status',function(status) {
         if(status.playerState == 'IDLE') {
+            on_media_finished();
             if(chromeCastplaying) {
                 clearInterval(ChromecastInterval);
                 chromeCastplaying = false;
             }
-            on_media_finished();
         } else if(status.playerState == 'PLAYING' || status.playerState == 'BUFFERING') {
             $('.mejs-overlay-button,.mejs-overlay,.mejs-overlay-loading,.mejs-overlay-play').hide()
             $('#fbxMsg2,#fbxMsg').remove();
             chromeCastplaying = true;
+            upnpContinuePlay = true;
             updateProgressBar();
             if(currentMedia.cover) {
-                $('.mejs-container').append('<div id="fbxMsg2" style="height:calc(100% - 60px);"><div style="top:50%;position: relative;"><img style="margin-left: 50%;left: -200px;position: relative;top: 50%;margin-top: -200px;width:400px;max-height:400px;" src="'+currentMedia.cover+'" /><h3 style="font-weight:bold;text-align: center;">'+currentMedia.title+'</h3></div></div>');
-                $('.mejs-container').append('<p id="fbxMsg" style="height:100px !important;position: absolute;top: 50%;margin: 0 50%;color: white;font-size: 30px;text-align: center;z-index: 10000;width: 100%;right: 50%;left: -50%;top: calc(50% - 350px);">'+_("Playing on your Chromecast device !")+'</p>')
+                $('.mejs-container').append('<div id="fbxMsg2" style="height:calc(100% - 60px);"><div style="top:50%;position: relative;"><img style="margin-left: 50%;left: -100px;position: relative;top: 50%;margin-top: -100px;width:200px;max-height:200px;" src="'+currentMedia.cover+'" /><h3 style="font-weight:bold;text-align: center;">'+currentMedia.title+'</h3></div></div>');
+                $('.mejs-container').append('<p id="fbxMsg" style="height:45px !important;position: absolute;top: 50%;margin: 0 50%;color: white;font-size: 30px;text-align: center;z-index: 10000;width: 100%;right: 50%;left: -50%;top: calc(50% - 200px);">'+_("Playing on your Chromecast device !")+'</p>')
             } else {
-                $('.mejs-container').append('<p id="fbxMsg" style="height:100px !important;position: absolute;top: 45%;margin: 0 50%;color: white;font-size: 30px;text-align: center;z-index: 10000;width: 100%;right: 50%;left: -50%;">'+currentMedia.title+' <br/> '+_("Playing on your Chromecast device !")+'</p>')
+                $('.mejs-container').append('<p id="fbxMsg" style="height:45px !important;position: absolute;top: 45%;margin: 0 50%;color: white;font-size: 30px;text-align: center;z-index: 10000;width: 100%;right: 50%;left: -50%;">'+currentMedia.title+' <br/> '+_("Playing on your Chromecast device !")+'</p>')
             }
         } else if(status.playerState == 'BUFFERING') {
             $('#fbxMsg2').remove();
