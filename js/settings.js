d5ceb8e0474840213a60da0ddbe0771ab246102f
Laguillaumie sylvain <sylvain@batexpert.fr>
2016-10-11 11:41:25 +0200

    fix osx modules

--- js/settings.js
+++ js/settings.js
@@ -1,15 +1,15 @@
-// Copyright (C) 
+// Copyright (C)
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -18,7 +18,7 @@
 var gui = require('nw.gui');
 console.log(gui)
 var win = gui.Window.get();
- 
+ var nodeip = require('ip')
 // create default settings or load from localstorage
 if (localStorage.StdSettings === undefined) {
 	settings.ipaddress = nodeip.address();
@@ -91,7 +91,7 @@ function setLocale() {
 }
 
 //get os type
-function getOsType() {  
+function getOsType() {
     var arch = process.arch;
     if (process.platform === 'win32') {
         return 'windows';
@@ -157,6 +157,6 @@ function setResolution() {
 }
 
 
-//start 
+//start
 setLocale();
 setResolution();
